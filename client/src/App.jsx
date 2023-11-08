f0ede741f2b67efd6ab4472de9d8a5ee1113ab3d
francis <francismainakabutha@gmail.com>
2023-11-08 11:05:00 +0300

    complete sign-up page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
